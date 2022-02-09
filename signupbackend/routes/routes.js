b4f6ad6f13182465ff9acb96765ac7e9561b8e48
Liam McLaughlin <84680661+liamandaidan@users.noreply.github.com>
2022-02-08 23:32:53 -0700

    Salt/Hash Pw

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignupModels')
+const bcrypt = require('bcrypt')
 
+router.post('/signup', async (request, response) => {
 
-router.post('/signup', (request, response) => {
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
     const signedUpUser = new signUpTemplateCopy({
         fullName: request.body.fullName,
         username: request.body.username,
         email: request.body.email,
-        password: request.body.password
+        password: securePassword
     })
     signedUpUser.save()
-    .then(data =>{
-        response.json(data)
-    })
-    .catch(error =>{
-        response.json(error)
-    })
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        })
 })
 
 module.exports = router
